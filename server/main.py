2c2d17f444b416d1dc63b9631292d9b38e607d7c
Apdbnir <ruslankorolev31122005@gmail.com>
2026-03-04 09:34:13 +0300

    Fix: Add localhost:3002 to CORS allowed origins

--- server/main.py
+++ server/main.py
@@ -29,7 +29,7 @@ app.include_router(websocket_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:3001", "http://localhost:8080", "http://localhost:8000"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:8080", "http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
