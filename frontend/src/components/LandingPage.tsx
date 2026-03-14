0a05a65e97221bfc6d37f1b4eada3d5707487734
Apdbnir <ruslankorolev31122005@gmail.com>
2026-03-14 09:17:07 +0300

    "feat:Add-more-background-orbs-make-them-dimmer

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -121,6 +121,10 @@ export const LandingPage: React.FC<LandingPageProps> = ({ onShowProfile }) => {
                     <div className="orb orb-2"></div>
                     <div className="orb orb-3"></div>
                     <div className="orb orb-4"></div>
+                    <div className="orb orb-5"></div>
+                    <div className="orb orb-6"></div>
+                    <div className="orb orb-7"></div>
+                    <div className="orb orb-8"></div>
                 </div>
             </div>
 
